301085904b862572324052993384cb86b834327d
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-03 13:07:53 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MyCarousel from "./SwiperCarousel/MyCarousel";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <MyCarousel />
     </div>
   );
 }
---
+++ src/SwiperCarousel/MyCarousel.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Swiper from "swiper";
+// CSS
+import "swiper/css/swiper.min.css";
+import "./MyCarousel.css";
+
+class MyCarousel extends Component {
+  componentDidMount() {
+    this.swiper = new Swiper(".swiper-container", {
+      grabCursor: true,
+    });
+  }
+  render() {
+    return (
+      <div className="swiper-container">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">Slide 1</div>
+          <hr />
+          <div className="swiper-slide">Slide 2</div>
+          <hr />
+          <div className="swiper-slide">Slide 3</div>
+          <hr />
+          <div className="swiper-slide">Slide 4</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MyCarousel;
