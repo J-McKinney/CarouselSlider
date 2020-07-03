7040e9f9d5e40580f5459bc4b1d952f476ce0f85
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-03 15:05:21 -0400

    finally got the correct css to work for me onhover the arrows will appear

--- src/CoverFlowCarousel/CoverFlowCarousel.jsx
+++ src/CoverFlowCarousel/CoverFlowCarousel.jsx
@@ -1,26 +1,22 @@
 import React, { Component } from "react";
 import Swiper from "swiper";
 // CSS
+
 //swiper css must come first
 import "swiper/css/swiper.min.css";
-// your custom css must come second to overwrite
-// certain stylings in swiper.css
+// your custom css must come second to overwrite certain stylings in swiper.css
 import "./CoverFlowCarousel.css";
 
-class MyCarousel extends Component {
+class CoverFlowCarousel extends Component {
   componentDidMount() {
     this.swiper = new Swiper(".swiper-container", {
-      // little hand cursor over slides
-      grabCursor: true,
-      // allows the slides to loop from the last slide back to the first in that direction
-      loop: true,
-      // helps to center the slides
-      centeredSlides: true,
-      // allows the slide you're looking at to be the centered slide with the slide before
-      // and the slide after to be hanging just off the page from the left and right of it
-      slidesPerView: 2,
+      speed: 800, // controls the speed between each slide when arrow click or grabCursor
+      grabCursor: true, // little hand cursor over slides
+      loop: true, // allows the slides to loop from the last slide back to the first in that direction
+      centeredSlides: true, // helps to center the slides
+      slidesPerView: 2, // allows the slide you're looking at to be the centered slide with the slide before and the slide after to be hanging just off the page from the left and right of it
       // COME BACK TO THE PARALLAX PORTION
-      //   parallax: true,
+      parallax: true, // Helps focus the users attention to the slide in front/center
       effect: "coverflow",
       coverflowEffect: {
         rotate: 50, // Slide rotate in degrees
@@ -41,19 +37,21 @@ class MyCarousel extends Component {
   }
   render() {
     return (
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">Cover Flow Slide 1</div>
-          <div className="swiper-slide">Cover Flow Slide 2</div>
-          <div className="swiper-slide">Cover Flow Slide 3</div>
-          <div className="swiper-slide">Cover Flow Slide 4</div>
+      <>
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide">Cover Flow Slide 1</div>
+            <div className="swiper-slide">Cover Flow Slide 2</div>
+            <div className="swiper-slide">Cover Flow Slide 3</div>
+            <div className="swiper-slide">Cover Flow Slide 4</div>
+          </div>
+          <div className="swiper-pagination" />
+          <div className="swiper-button-prev" />
+          <div className="swiper-button-next" />
         </div>
-        <div className="swiper-pagination" />
-        <div className="swiper-button-prev" />
-        <div className="swiper-button-next" />
-      </div>
+      </>
     );
   }
 }
 
-export default MyCarousel;
+export default CoverFlowCarousel;
